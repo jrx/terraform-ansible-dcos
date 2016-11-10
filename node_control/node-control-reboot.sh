e82e00c97a4087ad30f0acab0ad210571e1ed170
Makman2 <makman@alice.de>
2016-11-10 15:31:18 +0100

    node-control: Add reboot
    Reboots all connected nodes.

---
+++ node_control/node-control-reboot.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+
+# Parse parameters
+while [ $# -ge 1 ]; do
+    case $1 in
+        -y|--yes)
+            dont_ask=true
+            shift
+            ;;
+        *)
+            # FIXME Check if node can be found in configuration.
+            # FIXME Allow to pass synonyms, e.g. allow:
+            #       master1, m1, agent3, a3, agent_public12, p12, public_agent12
+            #       public-agent12, agent-public12, ap12, pa12
+            specified_nodes+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$dont_ask" ]; then
+    if [ ${#specified_nodes[@]} -eq 0 ]; then
+        count_string="all"
+    else
+        count_string="${#specified_nodes[@]}"
+    fi
+
+    read -p "Are you sure you want to reboot $count_string device(s) [y/n]? " choice
+    if [ "$choice" != "y" ] && [ "$choice" != "Y" ] && [ "$choice" != "yes" ]; then
+        exit
+    fi
+fi
+
+while read node; do
+    echo "Rebooting $node..."
+    ssh -n root@${node} reboot &
+done <<< "$nodes"
+wait
