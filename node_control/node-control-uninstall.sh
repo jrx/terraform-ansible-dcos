1c1eea60dc6d832cc02a95f6254bae434a041ca0
Makman2 <makman@alice.de>
2016-11-15 15:48:40 +0100

    node-control uninstall: Reactivate eth0
    eth0 interface gets somehow disabled after uninstall. Sadly the internet
    connection goes away, that's why we need to turn it on again.

--- node_control/node-control-uninstall.sh
+++ node_control/node-control-uninstall.sh
@@ -3,3 +3,12 @@ set -e
 
 cd ..
 ansible-playbook uninstall-all.yml -u root
+
+# Restore eth0 network interface again, as it's turned inactive somehow by
+# uninstall.
+echo "Restoring eth0 network interface..."
+while read node; do
+    ssh -n root@${node} "nmcli c up eth0" &
+done <<< "$nodes"
+
+wait
