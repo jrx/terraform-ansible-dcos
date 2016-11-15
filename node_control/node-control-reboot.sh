1d0784c293e61dc05c3edbbed117760b60396c67
Makman2 <makman@alice.de>
2016-11-15 16:03:01 +0100

    node-control reboot: Add help

--- node_control/node-control-reboot.sh
+++ node_control/node-control-reboot.sh
@@ -1,39 +1,51 @@
 #!/usr/bin/env bash
 set -e
 
-# Parse parameters
-while [ $# -ge 1 ]; do
-    case $1 in
-        -y|--yes)
-            dont_ask=true
-            shift
-            ;;
-        *)
-            # FIXME Check if node can be found in configuration.
-            # FIXME Allow to pass synonyms, e.g. allow:
-            #       master1, m1, agent3, a3, agent_public12, p12, public_agent12
-            #       public-agent12, agent-public12, ap12, pa12
-            specified_nodes+=("$1")
-            shift
-            ;;
-    esac
-done
+case "$1" in
+    help)
+        echo "node-control reboot [-y] [NODES]"
+        echo
+        echo "Reboots all connected nodes or all specified NODES."
+        echo
+        echo "-y, --yes"
+        echo "    Don't ask for confirmation, just reboot."
+        ;;
+    *)
+        # Parse parameters
+        while [ $# -ge 1 ]; do
+            case "$1" in
+                -y|--yes)
+                    dont_ask=true
+                    shift
+                    ;;
+                *)
+                    # FIXME Check if node can be found in configuration.
+                    # FIXME Allow to pass synonyms, e.g. allow:
+                    #       master1, m1, agent3, a3, agent_public12, p12, public_agent12
+                    #       public-agent12, agent-public12, ap12, pa12
+                    specified_nodes+="$1"$'\n'
+                    shift
+                    ;;
+            esac
+        done
 
-if [ -z "$dont_ask" ]; then
-    if [ ${#specified_nodes[@]} -eq 0 ]; then
-        count_string="all"
-    else
-        count_string="${#specified_nodes[@]}"
-    fi
+        if [ -z "$dont_ask" ]; then
+            if [ ${#specified_nodes[@]} -eq 0 ]; then
+                count_string="all"
+            else
+                count_string="${#specified_nodes[@]}"
+            fi
 
-    read -p "Are you sure you want to reboot $count_string device(s) [y/n]? " choice
-    if [ "$choice" != "y" ] && [ "$choice" != "Y" ] && [ "$choice" != "yes" ]; then
-        exit
-    fi
-fi
+            read -p "Are you sure you want to reboot $count_string device(s) [y/n]? " choice
+            if [ "$choice" != "y" ] && [ "$choice" != "Y" ] && [ "$choice" != "yes" ]; then
+                exit
+            fi
+        fi
 
-while read node; do
-    echo "Rebooting $node..."
-    ssh -n root@${node} reboot &
-done <<< "$nodes"
-wait
+        while read node; do
+            echo "Rebooting $node..."
+            ssh -n root@${node} reboot &
+        done <<< "$nodes"
+        wait
+        ;;
+esac
