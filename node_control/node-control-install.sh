d1afaa455a4171c221de94a81e3edd6d2592aa8f
Makman2 <makman@alice.de>
2016-11-24 12:43:20 +0100

    node-control install: Execute 'dcos auth login'
    So we authenticate with our cluster and can use demos and other useful
    stuff from node-control.

--- node_control/node-control-install.sh
+++ node_control/node-control-install.sh
@@ -3,3 +3,12 @@ set -e
 
 cd ..
 ansible-playbook install.yml -u root
+
+# Authenticate via DC/OS CLI.
+while read master; do
+    # Configure to point to first master.
+    dcos config set core.dcos_url http://1.0.10.1
+    break
+done <<< "$masters"
+
+dcos auth login
