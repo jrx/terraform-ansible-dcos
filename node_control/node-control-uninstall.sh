6f51a2e212b89e309f484aff3623ab36370a101c
Makman2 <mak95@arcor.de>
2016-12-13 19:59:20 +0100

    node-control uninstall: Add help

--- node_control/node-control-uninstall.sh
+++ node_control/node-control-uninstall.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-cd ..
-ansible-playbook uninstall-all.yml -u root
+case $1 in
+    help)
+        cat node-control-uninstall-help.txt
+        ;;
+    "")
+        cd ..
+        ansible-playbook uninstall-all.yml -u root
 
-# Restore eth0 network interface again, as it's turned inactive somehow by
-# uninstall.
-echo "Restoring eth0 network interface..."
-while read node; do
-    ssh -n root@${node} "nmcli c up eth0" &
-done <<< "$nodes"
+        # Restore eth0 network interface again, as it's turned inactive somehow
+        # by uninstall.
+        echo "Restoring eth0 network interface..."
+        while read node; do
+            ssh -n root@${node} "nmcli c up eth0" &
+        done <<< "$nodes"
 
-wait
+        wait
+        ;;
+    *)
+        errecho "Invalid argument supplied: $1"
+        exit 1
+        ;;
+esac
