2fa32e80f38e0d3b9aac78f2f0987da897b7ed8f
Makman2 <mak95@arcor.de>
2016-12-13 19:50:21 +0100

    node-control ping: Add help

--- node_control/node-control-ping.sh
+++ node_control/node-control-ping.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-# Check if each node is reachable from each other.
+case $1 in
+    help)
+        cat node-control-ping-help.txt
+        ;;
+    "")
+        set +e
+        while read node; do
+            ssh root@${node} "bash -s" < ./remote-ping.sh "'$node' '$nodes'" &
+        done <<< "$nodes"
+        set -e
 
-set +e
-while read node; do
-    ssh root@${node} "bash -s" < ./remote-ping.sh "'$node' '$nodes'" &
-done <<< "$nodes"
-set -e
-
-wait
+        wait
+        ;;
+    *)
+        errecho "Invalid argument supplied: $1"
+        exit 1
+        ;;
+esac
