17d05781cb1b3b09532274283bb3b8d0e75e2eef
Makman2 <mak95@arcor.de>
2016-12-13 19:58:53 +0100

    node-control system-update: Add help

--- node_control/node-control-system-update.sh
+++ node_control/node-control-system-update.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-# Update nodes.
+case $1 in
+    help)
+        cat node-control-system-update-help.txt
+        ;;
+    "")
+        while read node; do
+            echo "Requesting package updates for $node..."
+            ssh -n root@${node} "yum update -y" >/dev/null &
+        done <<< "$nodes"
 
-while read node; do
-    echo "Requesting package updates for $node..."
-    ssh -n root@${node} "yum update -y" >/dev/null &
-done <<< "$nodes"
+        wait
 
-wait
-
-echo "All nodes up-to-date."
+        echo "All nodes up-to-date."
+        ;;
+    *)
+        errecho "Invalid argument supplied: $1"
+        exit 1
+        ;;
+esac
