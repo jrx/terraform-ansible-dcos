fa7d74d470b1175f6298f6422c0f2ca7c9923b3c
Makman2 <mak95@arcor.de>
2016-12-13 19:48:09 +0100

    node-control install: Add help

--- node_control/node-control-install.sh
+++ node_control/node-control-install.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-cd ..
-ansible-playbook install.yml -u root
+case $1 in
+    help)
+        cat node-control-install-help.txt
+        ;;
+    "")
+        cd ..
+        ansible-playbook install.yml -u root
 
-# Authenticate via DC/OS CLI.
-while read master; do
-    # Configure to point to first master.
-    dcos config set core.dcos_url http://${master}
-    break
-done <<< "$masters"
+        # Authenticate via DC/OS CLI.
+        while read master; do
+            # Configure to point to first master.
+            dcos config set core.dcos_url http://${master}
+            break
+        done <<< "$masters"
 
-dcos auth login
+        dcos auth login
+        ;;
+    *)
+        errecho "Invalid argument supplied: $1"
+        exit 1
+        ;;
+esac
