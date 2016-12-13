cd0a7985cdb42895a4f158f26e6b0fc69945ab8c
Makman2 <mak95@arcor.de>
2016-12-13 19:49:49 +0100

    node-control nodes: Add help

--- node_control/node-control-nodes.sh
+++ node_control/node-control-nodes.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Master nodes:"
-cat <<< "$masters"
-echo
+case $1 in
+    help)
+        cat node-control-nodes-help.txt
+        ;;
+    "")
+        echo "Master nodes:"
+        cat <<< "$masters"
+        echo
 
-echo "Agent nodes (private):"
-cat <<< "$agents"
-echo
+        echo "Agent nodes (private):"
+        cat <<< "$agents"
+        echo
 
-echo "Agent nodes (public):"
-cat <<< "$agents_public"
-echo
+        echo "Agent nodes (public):"
+        cat <<< "$agents_public"
+        echo
+        ;;
+    *)
+        errecho "Invalid argument supplied: $1"
+        exit 1
+        ;;
+esac
